b01ba6a0b1ff4d4d7cda46b29d84721581e0516d
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-08 01:05:52 -0300

    metodo para capturar as propriedades presentes nos arquivos contendo dados necessarios para a conexao com o banco

--- src/main/java/com/github/nogueiralegacy/construcao/utils/Utils.java
+++ src/main/java/com/github/nogueiralegacy/construcao/utils/Utils.java
@@ -1,17 +1,20 @@
 package com.github.nogueiralegacy.construcao.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 public class Utils {
     /**
      * Retorna o caminho (Path) para o recurso especificado pelo nome do arquivo.
      * O recurso é procurado usando o ClassLoader associado à classe atual.
      *
-     * @param fileName o nome do arquivo do recurso a ser localizado
-     * @return o caminho (Path) para o recurso encontrado
-     * @throws IllegalArgumentException se o recurso não for encontrado
+     * @param fileName o nome do arquivo do recurso a ser localizado.
+     * @return o caminho (Path) para o recurso encontrado.
+     * @throws IllegalArgumentException se o recurso não for encontrado.
      */
     public Path getPath(String fileName) {
         ClassLoader classLoader = this.getClass().getClassLoader();
@@ -23,4 +26,25 @@ public class Utils {
         }
     }
 
+    /**
+     * Retorna as propriedades armazenadas em um arquivo especificado
+     * pelo seu caminho (pathPropertiesFile)
+     *
+     * @param pathPropertiesFile o caminho do arquivo a ser localizado.
+     * @return um objeto Properties com as propriedades contidas no arquivo
+     * ou null caso o arquivo não for encontrado.
+     *
+     */
+    public Properties getProperties(Path pathPropertiesFile) {
+        Properties properties = new Properties();
+
+
+        try (FileInputStream fileInputStream = new FileInputStream(pathPropertiesFile.toFile())) {
+            properties.load(fileInputStream);
+        } catch (IOException ioException) {
+            return properties;
+        }
+
+        return properties;
+    }
 }

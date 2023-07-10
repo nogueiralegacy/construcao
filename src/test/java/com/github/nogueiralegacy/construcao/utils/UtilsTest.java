8ecb3afaf47b3902d68cda89025942e4678c6cfd
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-10 16:31:21 -0300

    testes para o metodo getProperties

--- src/test/java/com/github/nogueiralegacy/construcao/utils/UtilsTest.java
+++ src/test/java/com/github/nogueiralegacy/construcao/utils/UtilsTest.java
@@ -1,12 +1,16 @@
 package com.github.nogueiralegacy.construcao.utils;
 
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.Properties;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilsTest {
 
     @Test
-    void nomeDeArquivoInexistenteInvalido() {
+    void arquivoNaoEncontradoNoResources() {
         // O nome de um arquivo que não existe
         String fileName = "inexistente";
 
@@ -16,12 +20,41 @@ class UtilsTest {
     }
 
     @Test
-    void nomeDeArquivoExistente() {
+    void arquivoEncontradoNoResources() {
         // O nome de um arquivo que existe
         String fileName = "config.properties";
 
         Utils utils = new Utils();
 
         assertNotNull(utils.getPath(fileName));
+    }
+
+    @Test
+    void propriedadesCarregadasCorretamente() {
+        String fileName = "config.properties";
+        //Propriedades esperadas
+        String urlBanco = "url_do_banco";
+        String usarioBanco = "seu_usuario";
+        String senhaBanco = "sua_senha";
+
+        Path pathProperties = new Utils().getPath(fileName);
+
+        Properties properties = Utils.getProperties(pathProperties);
+
+        assertEquals(urlBanco, properties.getProperty("db.url"));
+        assertEquals(usarioBanco, properties.getProperty("db.username"));
+        assertEquals(senhaBanco, properties.getProperty("db.password"));
+    }
+
+    @Test
+    void carregandoPropertiesComArquivoNaoEncontradoNoResources() {
+        // Não existe arquivo no resources com esse nome
+        String fileName = "inexistente";
+
+        Path pathProperties = new Utils().getPath(fileName);
+
+        Properties properties = Utils.getProperties(pathProperties);
+
+        assertTrue(properties.isEmpty());
     }
 }

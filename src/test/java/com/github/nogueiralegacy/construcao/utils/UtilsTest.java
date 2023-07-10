ec42d4c4d4cc966ffe1e0829bcce0c7ea5428121
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-10 14:11:44 -0300

    testes do metodo getPath

--- src/test/java/com/github/nogueiralegacy/construcao/utils/UtilsTest.java
+++ src/test/java/com/github/nogueiralegacy/construcao/utils/UtilsTest.java
@@ -1,5 +1,27 @@
 package com.github.nogueiralegacy.construcao.utils;
 
-public class UtilsTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class UtilsTest {
+
+    @Test
+    void nomeDeArquivoInexistenteInvalido() {
+        // O nome de um arquivo que não existe
+        String fileName = "inexistente";
+
+        Utils utils = new Utils();
+
+        assertNull(utils.getPath(fileName));
+    }
+
+    @Test
+    void nomeDeArquivoExistente() {
+        // O nome de um arquivo que existe
+        String fileName = "config.properties";
+
+        Utils utils = new Utils();
+
+        assertNotNull(utils.getPath(fileName));
+    }
 }

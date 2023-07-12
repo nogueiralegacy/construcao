86bc2344ced5a8152e1d85621753c4819a1d7c15
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-11 23:46:39 -0300

    Interface que declara as querys para consulta da tabela usuario

---
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/entities/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package com.github.nogueiralegacy.construcao.banco.entities;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Interface para declarar, de forma simples, consultas (query)
+ * ao banco de dados relacionadas a tabela Usuario.
+ */
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+    /**
+     * Busca um Usuario pelo seu username
+     *
+     * @param username username do usuario a ser buscado
+     * @return usuario com o username informado
+     */
+    Usuario findByUsername(String username);
+}
+

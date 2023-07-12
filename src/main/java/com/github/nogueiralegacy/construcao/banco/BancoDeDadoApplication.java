e4a4bbd65dae97b6611a4d68869156c26f9171fe
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-12 01:02:04 -0300

    arquivo de inicializacao do aplicacao spring boot

---
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/BancoDeDadoApplication.java
@@ -0,0 +1,12 @@
+package com.github.nogueiralegacy.construcao.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeDadoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BancoDeDadoApplication.class, args);
+    }
+}
+

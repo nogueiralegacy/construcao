1547517a27f17a6c60825b7d481ae18d2bceff64
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-12 20:00:40 -0300

    reestruturacao em diretorio repository e dos objetos que mapeiam para as tabelas no banco

--- src/main/java/com/github/nogueiralegacy/construcao/banco/repository/UsuarioRepository.java
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
-package com.github.nogueiralegacy.construcao.banco.entities;
+package com.github.nogueiralegacy.construcao.banco.repository;
 
+import com.github.nogueiralegacy.construcao.banco.orm.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 /**

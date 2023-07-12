12aa69bee568a3edb70f21527ee4c3b2a2879f85
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-12 01:00:03 -0300

    lombok getter e setters ignorando o atributo id, ja que a criacao e cuidado com esse atributo é feito pelo sgbd

--- src/main/java/com/github/nogueiralegacy/construcao/banco/entities/Usuario.java
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/entities/Usuario.java
@@ -2,12 +2,16 @@ package com.github.nogueiralegacy.construcao.banco.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @Entity
 @Table(name = "usuario")
 public class Usuario {
-
+    
+    @Getter(onMethod_ = @Deprecated)
+    @Setter(onMethod_ = @Deprecated)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;

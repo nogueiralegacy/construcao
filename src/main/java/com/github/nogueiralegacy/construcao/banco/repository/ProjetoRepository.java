3d336039427737b8f1a63b63ddadaf1a94c598f6
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-12 20:06:47 -0300

    interface de crud para declarar as querys que manipulam os dados da tabela projeto

---
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/repository/ProjetoRepository.java
@@ -0,0 +1,8 @@
+package com.github.nogueiralegacy.construcao.banco.repository;
+
+import com.github.nogueiralegacy.construcao.banco.orm.Projeto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjetoRepository extends CrudRepository<Projeto, Long> {
+
+}

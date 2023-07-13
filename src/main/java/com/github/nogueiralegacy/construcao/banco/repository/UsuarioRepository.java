2222ce008ff51d7ad26f391c84eb6b866857f478
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-13 17:40:14 -0300

    mudanca de nome de variaveis

--- src/main/java/com/github/nogueiralegacy/construcao/banco/repository/UsuarioRepository.java
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/repository/UsuarioRepository.java
@@ -12,17 +12,17 @@ public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     /**
      * Busca um Usuario pelo seu username
      *
-     * @param username username do usuario a ser buscado
+     * @param nickname username do usuario a ser buscado
      * @return usuario com o username informado
      */
-    Usuario findByUsername(String username);
+    Usuario findByNickname(String nickname);
 
     /**
      * Remove o usuario com o username informado
      *
-     * @param username usuario a ser removido do banco
+     * @param nickname usuario a ser removido do banco
      * @return quantidades de linhas afetadas
      */
-    Long deleteByUsername(String username);
+    Long deleteByNickname(String nickname);
 }
 

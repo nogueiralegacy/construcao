52563c8425c9a71f2bcd13c7d78316fb372ddb6b
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-13 20:24:28 -0300

    melhorando a qualidade do nome da classe controller principal

--- src/main/java/com/github/nogueiralegacy/construcao/api/DefaultController.java
+++ src/main/java/com/github/nogueiralegacy/construcao/api/DefaultController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class DefaultController {
     @GetMapping("/")
     public String index() {
         return "Hello World!";

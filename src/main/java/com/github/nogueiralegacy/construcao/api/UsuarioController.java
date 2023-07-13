d4a886709a6df07c74d83c7b945a81f81274af5a
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-13 19:20:51 -0300

    Controller para o usuario, classe de contato com o meio exterior, que define os end points

--- src/main/java/com/github/nogueiralegacy/construcao/api/UsuarioController.java
+++ src/main/java/com/github/nogueiralegacy/construcao/api/UsuarioController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 public class UsuarioController {
     UsuarioRepository usuarioRepository;
 

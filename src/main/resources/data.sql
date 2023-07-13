710f3707aff211e8089e3b37c948debcb12e2d59
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-13 20:44:46 -0300

    Arquivo com dados ficticeis para povoar o banco, tabela usuario e configurando o arquivo application para executar os comandos sql do arquivo data.sql de forma apropriada

---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('naldobenny', 'senha123', 'Naldo Benny', 'naldo@example.com', 'naldo_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('melodyoficial', 'senha456', 'Melody', 'melody@example.com', 'melody_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('xuxameneghel', 'senha789', 'Xuxa Meneghel', 'xuxa@example.com', 'xuxa_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('anittaoficial', 'senha321', 'Anitta', 'anitta@example.com', 'anitta_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('lucaslucco', 'senha654', 'Lucas Lucco', 'lucaslucco@example.com', 'lucaslucco_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('marinaruybarbosa', 'senha987', 'Marina Ruy Barbosa', 'marinaruybarbosa@example.com', 'marinaruybarbosa_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('ivetesangalo', 'senha012', 'Ivete Sangalo', 'ivete@example.com', 'ivete_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('neymarjr', 'senha345', 'Neymar Jr', 'neymar@example.com', 'neymar_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('sabrinasato', 'senha678', 'Sabrina Sato', 'sabrinasato@example.com', 'sabrinasato_avatar.jpg');
+
+INSERT INTO usuario (nickname, password, nome, email, avatar)
+VALUES ('cauareymond', 'senha901', 'Cauã Reymond', 'caua@example.com', 'caua_avatar.jpg');

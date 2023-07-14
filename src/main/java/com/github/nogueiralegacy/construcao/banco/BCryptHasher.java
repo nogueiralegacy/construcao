73e4db7efac954ff8153cb78bb689bc2060b94ce
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-14 18:09:02 -0300

    Criando hasher para as senhas a serem armazenadas no banco

---
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/BCryptHasher.java
@@ -0,0 +1,20 @@
+package com.github.nogueiralegacy.construcao.banco;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.SecureRandom;
+
+public final class BCryptHasher {
+    private static final int strenght = 10;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(strenght, new SecureRandom());
+
+    public String encode(String password) {
+        return encoder.encode(password);
+    }
+
+    public boolean matches(String password, String hash) {
+        return encoder.matches(password, hash);
+    }
+
+}

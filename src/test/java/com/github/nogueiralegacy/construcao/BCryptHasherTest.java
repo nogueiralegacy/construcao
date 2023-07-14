df49f933361c18f484aacb186c8dc2dadb72b1cf
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-14 18:09:02 -0300

    Criando hasher para as senhas a serem armazenadas no banco

---
+++ src/test/java/com/github/nogueiralegacy/construcao/BCryptHasherTest.java
@@ -0,0 +1,16 @@
+package com.github.nogueiralegacy.construcao;
+
+import com.github.nogueiralegacy.construcao.banco.BCryptHasher;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BCryptHasherTest {
+    @Test
+     void test() {
+        BCryptHasher bCryptHasher = new BCryptHasher();
+        String hash = bCryptHasher.encode("test123");
+        System.out.println(hash);
+        assertTrue(bCryptHasher.matches("test123", hash));
+    }
+}

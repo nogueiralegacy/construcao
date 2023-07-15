d02535e9c9b7898f377c0a90511b860d0b90bb06
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-15 19:06:39 -0300

    adequando declaracao de variavel ao padrao

--- src/main/java/com/github/nogueiralegacy/construcao/banco/BCryptHasher.java
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/BCryptHasher.java
@@ -5,9 +5,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.security.SecureRandom;
 
 public final class BCryptHasher {
-    private static final int strenght = 10;
+    private static final int STRENGTH = 10;
 
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(strenght, new SecureRandom());
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(STRENGTH, new SecureRandom());
 
     public String encode(String password) {
         return encoder.encode(password);

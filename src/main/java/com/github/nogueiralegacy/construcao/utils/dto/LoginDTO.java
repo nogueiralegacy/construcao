e0f419fd999c511dcf6c7c2f4c67ce148584f9ce
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-17 21:19:18 -0300

    convertento login dto para um record

--- src/main/java/com/github/nogueiralegacy/construcao/utils/dto/LoginDTO.java
+++ src/main/java/com/github/nogueiralegacy/construcao/utils/dto/LoginDTO.java
@@ -2,20 +2,8 @@ package com.github.nogueiralegacy.construcao.utils.dto;
 
 import com.github.nogueiralegacy.construcao.api.security.Login;
 import com.github.nogueiralegacy.construcao.banco.orm.Usuario;
-import com.github.nogueiralegacy.construcao.banco.orm.UsuarioRole;
-import lombok.Getter;
 
-@Getter
-public class LoginDTO {
-    private final String username;
-    private final String password;
-    private final UsuarioRole role;
-
-    public LoginDTO(String username, String password, UsuarioRole role) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
+public record LoginDTO (String username, String password) {
 
     public static Login usuarioToLogin(Usuario usuario) {
         return new Login(usuario.getNickname(), usuario.getPassword(), usuario.getRole());

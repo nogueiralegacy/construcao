8751d578f02024f481c8c45da56d36cc992f54a6
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-17 21:21:33 -0300

    end points para autenticacao, login, register

--- src/main/java/com/github/nogueiralegacy/construcao/api/security/SecurityConfiguration.java
+++ src/main/java/com/github/nogueiralegacy/construcao/api/security/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.github.nogueiralegacy.construcao.api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,10 +21,17 @@ public class SecurityConfiguration {
                         .disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/usuario").hasRole("USER")
                         .requestMatchers(HttpMethod.GET, "/projeto").hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }

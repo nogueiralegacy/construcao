225a3c0fabbb2865be5ff463051906f88b20226d
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-18 10:25:07 -0300

    metodo que retorna a interface PasswordEncoder para o swpring boot usar na autenticacao do usuario

--- src/main/java/com/github/nogueiralegacy/construcao/banco/BCryptHasher.java
+++ src/main/java/com/github/nogueiralegacy/construcao/banco/BCryptHasher.java
@@ -1,19 +1,20 @@
 package com.github.nogueiralegacy.construcao.banco;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.security.SecureRandom;
 
-public final class BCryptHasher {
+public final class BCryptHasher implements PasswordEncoder {
     private static final int STRENGTH = 10;
 
     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(STRENGTH, new SecureRandom());
 
-    public String encode(String password) {
-        return encoder.encode(password);
+    public String encode(CharSequence rawPassword) {
+        return encoder.encode(rawPassword);
     }
 
-    public boolean matches(String password, String hash) {
-        return encoder.matches(password, hash);
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword, encodedPassword);
     }
 }

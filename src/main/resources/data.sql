1cd585c89033754e3d1396c6836f0b709fe52019
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-18 18:10:46 -0300

    dados para povoar o banco ja com a senha em hash, senha=${primeiro_nome}123

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,32 +1,29 @@
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('naldobenny', 'senha123', 'Naldo Benny', 'naldo@example.com', 'naldo_avatar.jpg', 'USER');
+VALUES ('naldobenny', '$2a$10$q4qhrlBIBc1Hzg6wcHVTr.OSsf05NI3jI3SieDSK4sUMr5NYsluVe', 'Naldo Benny', 'naldo@example.com', 'naldo_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('melodyoficial', 'senha456', 'Melody', 'melody@example.com', 'melody_avatar.jpg', 'USER');
+VALUES ('melodyoficial', '$2a$10$qWW88sUlD61O6Omu7A87KejsRpgB/ulrP88vSm90PHUQuiWEkFJNO', 'Melody', 'melody@example.com', 'melody_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('xuxameneghel', 'senha789', 'Xuxa Meneghel', 'xuxa@example.com', 'xuxa_avatar.jpg', 'USER');
+VALUES ('xuxameneghel', '$2a$10$tEJikbKsfKlgvO2sUQUEwOpGOpTHyhHmVwaIjKgRxEWlQx4Va0Z1e', 'Xuxa Meneghel', 'xuxa@example.com', 'xuxa_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('anittaoficial', 'senha321', 'Anitta', 'anitta@example.com', 'anitta_avatar.jpg', 'USER');
+VALUES ('anittaoficial', '$2a$10$ooY5UIl5Nc9jcXXScGJmd.NFf.9FswKcycHRwIW02VDhvUiLZ4BoC', 'Anitta', 'anitta@example.com', 'anitta_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('lucaslucco', 'senha654', 'Lucas Lucco', 'lucaslucco@example.com', 'lucaslucco_avatar.jpg', 'USER');
+VALUES ('lucaslucco', '$2a$10$d43g27mphfegkUessqreheQgPXeYntFm.BPDGblY5/KGnsl.ei2SK', 'Lucas Lucco', 'lucaslucco@example.com', 'lucaslucco_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('marinaruybarbosa', 'senha987', 'Marina Ruy Barbosa', 'marinaruybarbosa@example.com', 'marinaruybarbosa_avatar.jpg', 'USER');
+VALUES ('marinaruybarbosa', '$2a$10$DCZpb87zSHAVxjTa3z1Ur.LhSOKar9ApBG1fM2aDOYpUlUBrxej2a', 'Marina Ruy Barbosa', 'marinaruybarbosa@example.com', 'marinaruybarbosa_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('ivetesangalo', 'senha012', 'Ivete Sangalo', 'ivete@example.com', 'ivete_avatar.jpg', 'USER');
+VALUES ('ivetesangalo', '$2a$10$IVKt9CoxBuEamGl4D6567OV5.6ZNxmyInIgNmNCdag4TchL1HePem', 'Ivete Sangalo', 'ivete@example.com', 'ivete_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('neymarjr', 'senha345', 'Neymar Jr', 'neymar@example.com', 'neymar_avatar.jpg', 'USER');
+VALUES ('neymarjr', '$2a$10$ivfIjiWNoua.LboJRJPeu.ULPtb8yj61zCcNln38OwgoU6V4YI4X.', 'Neymar Jr', 'neymar@example.com', 'neymar_avatar.jpg', 'USER');
 
 INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('sabrinasato', 'senha678', 'Sabrina Sato', 'sabrinasato@example.com', 'sabrinasato_avatar.jpg', 'USER');
-
-INSERT INTO usuario (nickname, password, nome, email, avatar, role)
-VALUES ('cauareymond', 'senha901', 'Cauã Reymond', 'caua@example.com', 'caua_avatar.jpg', 'USER');
+VALUES ('sabrinasato', '$2a$10$XIr9WFKPJigp8MXs7cHg0OCstWAnOqqfwbzxrGcN.YasJ/Y9d6OzK', 'Sabrina Sato', 'sabrinasato@example.com', 'sabrinasato_avatar.jpg', 'USER');
 
 INSERT INTO projeto (nome, descricao, data_criacao, id_usuario)
 VALUES ('Construção', 'Projeto da disciplina de Construção de software', TO_TIMESTAMP('2023-07-14 12:34:56', 'YYYY-MM-DD HH24:MI:SS'), 1);

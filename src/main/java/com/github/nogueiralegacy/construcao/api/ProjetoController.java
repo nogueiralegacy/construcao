014f055169c58a88ab700ffe089806235cbb23f6
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-27 14:35:21 -0300

    Rota e operacao de salvar um projeto que foi enviado de forma correta, de acordo com o ProjetoDTo

--- src/main/java/com/github/nogueiralegacy/construcao/api/ProjetoController.java
+++ src/main/java/com/github/nogueiralegacy/construcao/api/ProjetoController.java
@@ -2,21 +2,36 @@ package com.github.nogueiralegacy.construcao.api;
 
 import com.github.nogueiralegacy.construcao.banco.orm.Projeto;
 import com.github.nogueiralegacy.construcao.banco.repository.ProjetoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.nogueiralegacy.construcao.service.ProjetoService;
+import com.github.nogueiralegacy.construcao.utils.dto.ProjetoDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/projeto")
 public class ProjetoController {
     private final ProjetoRepository projetoRepository;
+    private final ProjetoService projetoService;
 
-    public ProjetoController(ProjetoRepository projetoRepository) {
+    public ProjetoController(ProjetoRepository projetoRepository, ProjetoService projetoService) {
         this.projetoRepository = projetoRepository;
+        this.projetoService = projetoService;
     }
 
     @GetMapping
     public Iterable<Projeto> getProjetos() {
         return projetoRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity setProjeto(@RequestBody ProjetoDTO projetoDTO) {
+       try {
+           projetoService.saveProjeto(projetoDTO);
+       }
+       catch (Exception e) {
+           return ResponseEntity.badRequest().body(e.getMessage());
+       }
+
+        return ResponseEntity.ok().build();
     }
 }

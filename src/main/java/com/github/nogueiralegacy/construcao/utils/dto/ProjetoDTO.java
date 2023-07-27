ce7bed124fe82d97ef882847da3900745c7f7637
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-27 14:34:05 -0300

    Representacao de projeto que é recebida pela interface (API)

---
+++ src/main/java/com/github/nogueiralegacy/construcao/utils/dto/ProjetoDTO.java
@@ -0,0 +1,4 @@
+package com.github.nogueiralegacy.construcao.utils.dto;
+
+public record ProjetoDTO(String nome, String descricao, String dataCriacao, String criadorNickname, String[] participantesNicknames) {
+}

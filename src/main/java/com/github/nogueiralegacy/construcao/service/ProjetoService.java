4115f6512b155d9ea7a1a3ac3a53236ce53f2cd5
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-07-27 22:13:37 -0300

    melhora na validação e controle do fluxo das entradas para adicção de um projeto

--- src/main/java/com/github/nogueiralegacy/construcao/service/ProjetoService.java
+++ src/main/java/com/github/nogueiralegacy/construcao/service/ProjetoService.java
@@ -6,6 +6,8 @@ import com.github.nogueiralegacy.construcao.utils.Utils;
 import com.github.nogueiralegacy.construcao.utils.dto.ProjetoDTO;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeParseException;
+
 @Service
 public class ProjetoService {
     private final UsuarioService usuarioService;
@@ -17,13 +19,24 @@ public class ProjetoService {
     }
 
     public void saveProjeto(ProjetoDTO projetoDTO) throws IllegalArgumentException {
+        if (!isProjetoValido(projetoDTO)) {
+            throw new IllegalArgumentException("Formato de projeto inválido, os campos 'nome' e 'criadorNickname' são obrigatórios");
+        }
         Projeto projeto = new Projeto();
         projeto.setNome(projetoDTO.nome());
         projeto.setDescricao(projetoDTO.descricao());
-        projeto.setDataCriacao(Utils.toLocalDateTime(projetoDTO.dataCriacao()));
+        try {
+            projeto.setDataCriacao(Utils.toLocalDateTime(projetoDTO.dataCriacao()));
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Formato de data inválido, o formato correto é 'yyyy-MM-dd HH:mm:ss'");
+        }
         projeto.setCriador(usuarioService.findByNickname(projetoDTO.criadorNickname()));
         projeto.setParticipantes(usuarioService.findByNicknames(projetoDTO.participantesNicknames()));
 
         projetoRepository.save(projeto);
+    }
+
+    private boolean isProjetoValido(ProjetoDTO projetoDTO) {
+        return projetoDTO != null && projetoDTO.nome() != null && projetoDTO.criadorNickname() != null;
     }
 }

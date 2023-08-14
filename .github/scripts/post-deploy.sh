d6af540cc69092889f7ef2ee6cd5c067e81b4d4a
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-08-14 16:38:55 -0300

    configuração do CD

---
+++ .github/scripts/post-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Criação de um arquivo temporário para armazenar a chave privada
+echo "${{ secrets.SSH_PRIVATE_KEY }}" > key.pem
+chmod 400 key.pem
+
+# Início da conexão SSH e execução do comando para iniciar o JAR
+ssh -i key.pem -o StrictHostKeyChecking=no root@157.230.84.81 "nohup java -jar /aplicacao/construcao-1.0.jar > output.log 2>&1 &"
+
+# Remoção do arquivo temporário
+rm key.pem

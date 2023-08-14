e09d24f90378d1a1829eb50c30cc5952f6b73fa1
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-08-14 19:11:28 -0300

    devops

--- .github/scripts/post-deploy.sh
+++ .github/scripts/post-deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Criação de um arquivo temporário para armazenar a chave privada
-echo $SSH_PRIVATE_KEY > key.pem
+echo $SSH_PRIVATE_KEY > id_rsa
 chmod 400 key.pem
 
 # Início da conexão SSH e execução do comando para iniciar o JAR
-ssh -i key.pem root@157.230.84.81 "nohup java -jar /root/aplicacao/target/construcao-1.0.jar > output.log 2>&1 &"
+ssh -i id_rsa root@157.230.84.81 "nohup java -jar /root/aplicacao/target/construcao-1.0.jar > output.log 2>&1 &"
 
 # Remoção do arquivo temporário
-rm key.pem
+rm id_rsa

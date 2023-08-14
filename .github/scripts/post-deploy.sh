7e92a9d1143b8d147a2ebf86a0a3f4100cd8c250
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-08-14 18:57:51 -0300

    devops

--- .github/scripts/post-deploy.sh
+++ .github/scripts/post-deploy.sh
@@ -5,7 +5,7 @@ echo $SSH_PRIVATE_KEY > key.pem
 chmod 400 key.pem
 
 # Início da conexão SSH e execução do comando para iniciar o JAR
-ssh -i key.pem -o StrictHostKeyChecking=no root@157.230.84.81 "nohup java -jar /root/aplicacao/construcao-1.0.jar > output.log 2>&1 &"
+ssh -i key.pem -o StrictHostKeyChecking=no root@157.230.84.81 "nohup java -jar /root/aplicacao/target/construcao-1.0.jar > output.log 2>&1 &"
 
 # Remoção do arquivo temporário
 rm key.pem

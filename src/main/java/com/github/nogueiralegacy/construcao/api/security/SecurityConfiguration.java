679d5076acfeda7073fea2f203af47574881f34b
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-08-16 14:35:12 -0300

    funcionalidade para somente ADMIN registrarem outros usuarios

--- src/main/java/com/github/nogueiralegacy/construcao/api/security/SecurityConfiguration.java
+++ src/main/java/com/github/nogueiralegacy/construcao/api/security/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/projeto").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)

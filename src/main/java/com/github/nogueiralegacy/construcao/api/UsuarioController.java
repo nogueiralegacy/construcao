b3cd52fa1cb7be3c803bda622315eb6a86a8fe46
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-08-16 17:27:15 -0300

    melhoria nos retornos

--- src/main/java/com/github/nogueiralegacy/construcao/api/UsuarioController.java
+++ src/main/java/com/github/nogueiralegacy/construcao/api/UsuarioController.java
@@ -1,10 +1,7 @@
 package com.github.nogueiralegacy.construcao.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.nogueiralegacy.construcao.domain.Usuario;
 import com.github.nogueiralegacy.construcao.service.UsuarioService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +20,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/{nickname}")
-    public ResponseEntity<String> findByNickname(@PathVariable("nickname") String nickname) {
+    public ResponseEntity findByNickname(@PathVariable("nickname") String nickname) {
         Usuario usuario;
         try {
             usuario = usuarioService.findByNickname(nickname);
@@ -31,14 +28,6 @@ public class UsuarioController {
             return ResponseEntity.badRequest().body("Erro ao buscar usuario: " + e.getMessage());
         }
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String usuarioJson;
-        try {
-            usuarioJson = objectMapper.writeValueAsString(usuario);
-        } catch (JsonProcessingException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao converter usuario para JSON");
-        }
-
-        return ResponseEntity.ok(usuarioJson);
+        return ResponseEntity.ok(usuario);
     }
 }
